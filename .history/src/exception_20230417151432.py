e0032fb523cec256601b948106e3615426ae888a
HridayNagrani <hridaynagrani@gmail.com>
2023-04-18 08:43:26 +0530

    EDA and Problem Statement

--- .history/src/exception_20230417151432.py
+++ .history/src/exception_20230417151432.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+import src.logger logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #Info about error like file and line number will be in exc_tb
